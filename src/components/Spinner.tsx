9e352ee2fe62171e21dc3746d10be97821ea337c
igorsaux <igorsa.ux@outlook.com>
2021-08-09 18:53:41 +0300

    добавлен спинер

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Spinner.scss'
+
+/**
+ * Пропсы для {@link Spinner}
+ */
+export interface SpinnerProps {
+  /**
+   * Сообщение.
+   */
+  text: string;
+}
+
+/**
+ * Спиннер.
+ */
+export const Spinner = (props: SpinnerProps) => {
+  return <div className='Spinner'>
+    <i className='Spinner__icon fas fa-spinner'>
+    </i><span className='Spinner__text'>{props.text}</span>
+  </div>
+}
