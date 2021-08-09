3eb2d03a432b1f886f4e61d14e2fc28fbbc41f45
igorsaux <igorsa.ux@outlook.com>
2021-08-09 15:59:43 +0300

    базовые компоненты, шапка чейнджлога

---
+++ src/components/Changelog.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { OnyxHeader } from './OnyxHeader'
+
+/**
+ * Чейнджлог
+ */
+export const Changelog = () => {
+  return <div className='Changelog'>
+    <OnyxHeader />
+  </div>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './styles/index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
