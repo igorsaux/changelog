74e22083b97a8a9d13520d00f29ba09c2f3ba876
igorsaux <igorsa.ux@outlook.com>
2021-08-11 20:57:31 +0300

    ониксзация и деплой

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,15 @@ import './styles/index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 
+console.log(`
+    ________                __  _      ____
+   / ____/ /_  ____ _____  / /_(_)____/ __ \\____  __  ___  __
+  / /   / __ \\/ __ \`/ __ \\/ __/ / ___/ / / / __ \\/ / / / |/_/
+ / /___/ / / / /_/ / /_/ / /_/ / /__/ /_/ / / / / /_/ />  <  
+ \\____/_/ /_/\\__,_/\\____/\\__/_/\\___/\\____/_/ /_/\\__, /_/|_|  
+                                               /____/        
+`)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
