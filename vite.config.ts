ca5e4da8b06f6d9629a0c37e7a43773f10234a57
igorsaux <igorsa.ux@outlook.com>
2021-09-03 01:34:38 +0300

    убрана поддержка ie11

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
   plugins: [
-    reactRefresh(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: [
-        'regenerator-runtime/runtime',
-        'whatwg-fetch'
-      ]
-    })
+    reactRefresh()
   ],
   mode: 'production'
 })
