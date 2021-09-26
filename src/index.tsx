cfa653c6b64d8cd570f3aa571053ed36b89073c9
igorsaux <igorsa.ux@outlook.com>
2021-09-26 16:57:11 +0300

    небольшие фиксы

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,12 @@ import { HashRouter } from 'react-router-dom'
 import './styles/index.css'
 import 'normalize.css/normalize.css'
 
-console.log(`
- ..|''||                             
-.|'    ||  .. ...   .... ... ... ... 
-||      ||  ||  ||   '|.  |   '|..'  
-'|.     ||  ||  ||    '|.|     .|.   
- ''|...|'  .||. ||.    '|    .|  ||. 
-                    .. |             
-                     ''              
+console.log('%cOnyx', `
+  color: #F34160;
+  font-weight: bold;
+  font-size: 12ch;
+  text-shadow: 0px 1px black, 0 2px black, 0 3px black, 0 4px black;
+  font-style: italic;
 `)
 
 ReactDOM.render(
