fd499c21ff4d47bbe3e10275f39b6898703c34fc
igorsaux <igorsa.ux@outlook.com>
2021-09-28 18:53:28 +0300

    фиксы позиционирования стрелки и цвета границ карточек

--- src/components/GitHubPopover.tsx
+++ src/components/GitHubPopover.tsx
@@ -132,7 +132,7 @@ function GitHubPopover (props: GitHubPopoverProps) {
 
   return <a href={props.prUrl} style={popoverStyle} ref={tooltipRef} className="GitHubPopover">
     <svg style={arrowStyle} className='GitHubPopover__Arrow' width="14" height="7" viewBox="0 0 14 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M7 0L0 7H14L7 0Z" fill="#E1E4E8"/>
+      <path d="M7 0L0 7H14L7 0Z" fill="#D0D7DE"/>
       <path d="M7 1.5L1.5 7H12.5L7 1.5Z" fill="white"/>
     </svg>
     <div className="GitHubPopover__Content" style={{
