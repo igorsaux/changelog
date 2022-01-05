773d12a69d53b6b73769ad1ed774b5f4897f694f
igorsaux <igorsa.ux@outlook.com>
2022-01-05 05:49:26 +0300

    Для превью ссылок на PR теперь используются OG изображения

--- src/components/GitHubPopover.tsx
+++ src/components/GitHubPopover.tsx
@@ -1,60 +1,9 @@
 import Tippy from '@tippyjs/react'
-import React, { useContext, useState } from 'react'
+import React, { createRef } from 'react'
 import 'tippy.js/dist/tippy.css'
 import 'tippy.js/animations/scale.css'
 import 'tippy.js/dist/border.css'
 import '../styles/GitHubPopover.scss'
-import { AppContext } from './App'
-import { GitHubRepository } from '../abstractions/GitHubRepository'
-import { GitHubPullRequest } from '../abstractions/GitHubPullRequest'
-
-const dateTimeFormat = new Intl.DateTimeFormat('ru-RU', {
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric'
-})
-
-function PullRequest (props: { pr: GitHubPullRequest }) {
-  const { pr } = props
-
-  return (
-    <div className='PullRequestCard'>
-      <div className='Repository'>
-        {pr.base.repo.owner.login}/{pr.base.repo.name}
-      </div>
-      <div className='Naming'>
-        <span className='Number'>#{pr.number}</span>{' '}
-        <span className='Title'>{pr.title}</span>
-      </div>
-      <div className='Misc'>
-        <img className='Avatar' src={pr.user.avatar_url} alt={pr.user.login} />
-        <span className='Author'>{pr.user.login}</span>
-        <span className='CloseDate'>
-          {' '}
-          • {dateTimeFormat.format(new Date(pr.closed_at))}
-        </span>
-      </div>
-    </div>
-  )
-}
-
-function PullRequestPlaceholder () {
-  return (
-    <div className='PullRequestCard PullRequestCard--Placeholder'>
-      <div className='Repository Repository--Placeholder'></div>
-      <div className='Naming Naming--Placeholder'></div>
-      <div className='Misc Misc--Placeholder'></div>
-    </div>
-  )
-}
-
-function PullRequestError () {
-  return (
-    <div className='PullRequestCard PullRequestCard--Error'>
-      😔 Гитхаб недоступен.
-    </div>
-  )
-}
 
 /**
  * Пропсы для {@link GitHubPopoverProps}
@@ -70,9 +19,7 @@ function GitHubPopover (props: GitHubPopoverProps) {
   const repositoryName = splittedUrl[4]
   const prNumber = splittedUrl[6]
 
-  const [isError, setIsError] = useState(false)
-  const [prData, setPrData] = useState<GitHubPullRequest | undefined>(undefined)
-  const { api } = useContext(AppContext)
+  const imageRef = createRef<HTMLImageElement>()
 
   return (
     <Tippy
@@ -80,41 +27,32 @@ function GitHubPopover (props: GitHubPopoverProps) {
       theme='light'
       interactive
       interactiveBorder={10}
-      maxWidth={300}
+      maxWidth={400}
       inertia
       delay={[0, 250]}
       touch={false}
       onShow={() => {
-        api
-          .PullRequest(
-            new GitHubRepository(repositoryName, repositoryOwner),
-            prNumber
-          )
-          .then(data => {
-            setIsError(false)
-            return setPrData(data)
-          })
-          .catch(_ => setIsError(true))
+        if (!imageRef.current) {
+          return
+        }
+
+        imageRef.current.src = `https://opengraph.githubassets.com/1/${repositoryOwner}/${repositoryName}/pull/${prNumber}`
       }}
       content={
         <a
           href={props.prUrl}
           rel='noreferrer'
           target='_blank'
-          className='GitHubPopover'>
-          {isError
-            ? (
-            <PullRequestError />
-              )
-            : prData
-              ? (
-            <PullRequest pr={prData} />
-                )
-              : (
-            <PullRequestPlaceholder />
-                )}
+          className='GitHubPopover'
+        >
+          <img
+            ref={imageRef}
+            src='https://opengraph.githubassets.com/1/'
+            alt=''
+          />
         </a>
-      }>
+      }
+    >
       {props.children}
     </Tippy>
   )
